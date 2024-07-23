a4a4e3e5667fda42f41bf5f835c0bac104001c03
Sulay <Sulaymank704@gmail.com>
2024-07-23 16:08:54 -0400

    file path changed

--- my-app/server.js
+++ my-app/server.js
@@ -125,11 +125,11 @@ app.post('/api/login', (req, res) => {
 
 
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the API!');
-});
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the API!');
+// });
 
-app.get('*', (req,res)=>{
+app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, './build/index.html'))
 })
 
